d49c41458146db5876b2f92ae55f7489b239d9ff
Seonghoon Oh <su5785@naver.com>
2022-03-01 13:36:24 +0900

    feat: Add GlobalStyle and theme for styled-components

--- src/App.js
+++
@@ -1,5 +0,0 @@
-const App = () => {
-  return <div>APP 컴포넌트 입니다.</div>;
-};
-
-export default App;
---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import GlobalStyle from "../styles/GlobalStyle";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <div>APP 컴포넌트 입니다.</div>;
+    </>
+  );
+};
+
+export default App;
