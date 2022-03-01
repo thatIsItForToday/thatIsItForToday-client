6dd695c366afc8d44602e030d62ddb85d57bcddc
Seonghoon Oh <su5785@naver.com>
2022-03-01 13:37:48 +0900

    feat: Add redux store and userReducer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import "./index.css";
-import App from "./App";
+import App from "./app/App";
+import store from "./app/store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
