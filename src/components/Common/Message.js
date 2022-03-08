aac34c727b1452570c4b95e29949a281c2493a64
Seonghoon Oh <su5785@naver.com>
2022-03-08 22:31:55 +0900

    refactor: Modify RecorderPage component UI UX logic

--- src/components/Common/Message.js
+++ src/components/Common/Message.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const Message = ({ message }) => {
-  return <MessageContainer>{message}</MessageContainer>;
+  const ref = useRef();
+
+  useEffect(() => {
+    const $message = ref.current;
+
+    setTimeout(() => {
+      $message.classList.add("show");
+    }, 0);
+
+    return () => {
+      $message.classList.remove("show");
+    };
+  }, []);
+
+  return (
+    <MessageContainer id="message" ref={ref}>
+      {message}
+    </MessageContainer>
+  );
 };
 
 Message.propTypes = {
@@ -14,7 +32,28 @@ const MessageContainer = styled.div`
   ${({ theme }) => theme.container.flexCenter};
   width: fit-content;
   padding: ${({ theme }) => theme.spacing.xxl};
+  background-color: transparent;
+  font-family: "ABeeZee";
+  font-weight: 500;
   font-size: ${({ theme }) => theme.fontSizes.xl};
+  opacity: 0;
+  transition: all 0.3s ease-in-out;
+
+  @keyframes appearAnimationText {
+    from {
+      opacity: 0;
+      transform: translateX(20px);
+    }
+    to {
+      opacity: 1;
+      transform: translateX(0);
+    }
+  }
+
+  &.show {
+    opacity: 1;
+    animation: appearAnimationText 0.3s ease-in-out;
+  }
 `;
 
 export default Message;
--- src/components/pages/RecorderPage.js
+++ src/components/pages/RecorderPage.js
@@ -1,8 +1,7 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 import axios from "axios";
-import ReactHlsPlayer from "react-hls-player/dist";
 
 import Recorder from "../Recorder";
 import {
@@ -10,15 +9,21 @@ import {
   getVideoStreamingURL,
   uploadToAWSS3,
 } from "../../utils/awsUtils";
+import { getToday } from "../../utils/dateUtils";
+import Message from "../Common/Message";
 
 const RecorderPage = () => {
   const { user: currentUser } = useSelector(state => state.user);
-  const { recorder } = useSelector(state => state.video);
+
   const [gif, setGif] = useState("");
   const [videoBlob, setVideoBlob] = useState("");
   const [gifBlob, setGifBlob] = useState("");
+  const [isUploading, setIsUploading] = useState(false);
+  const [isUploadSuccess, setIsUploadSuccess] = useState(false);
 
-  const handleSaveRecordedClick = useCallback(async () => {
+  const handleUploadRecordedClick = useCallback(async () => {
+    setIsUploading(true);
+
     try {
       const [videoUploadData, gifUploadData] = await Promise.all([
         getSignedURL(process.env.REACT_APP_GET_PRESIGNED_VIDEO_UPLOAD_URL),
@@ -40,38 +45,52 @@ const RecorderPage = () => {
         gifURL: gifUploadURL.split("?")[0],
       };
 
-      const result = await axios.post(
+      await axios.post(
         `${process.env.REACT_APP_BASE_API_URL}/users/${currentUser.id}/video`,
         uploadedURLs
       );
+
+      setIsUploading(false);
+      setIsUploadSuccess(true);
+      console.log("여기까지 오나");
     } catch (error) {
       console.log(error);
     }
   }, [videoBlob, gifBlob]);
 
+  const handleReTakeButtonClick = useCallback(() => {
+    setGif("");
+    setVideoBlob("");
+    setGifBlob("");
+  }, []);
+
   return (
     <Section>
-      <Recorder
-        setGif={setGif}
-        setVideoBlob={setVideoBlob}
-        setGifBlob={setGifBlob}
-      />
-      {recorder?.isRecording && <div>녹화를 마치고 있습니다...</div>}
-      {gif && (
-        <Container>
+      {gif ? (
+        <Container className={gif ? "show" : "no-show"}>
+          <Text>{`Your memory on ${getToday()}`}</Text>
           <Gif src={gif} />
-          <Button onClick={handleSaveRecordedClick}>저장</Button>
+          <ButtonContainer>
+            {isUploading && (
+              <Message message="Your memory is being uploaded.." />
+            )}
+            {!isUploading && !isUploadSuccess && (
+              <>
+                <Button onClick={handleUploadRecordedClick}>Upload</Button>
+                <Button onClick={handleReTakeButtonClick}>Re-take</Button>
+              </>
+            )}
+          </ButtonContainer>
+          {isUploadSuccess && <Message message="Uploaded successfully!" />}
         </Container>
+      ) : (
+        <Recorder
+          gif={gif}
+          setGif={setGif}
+          setVideoBlob={setVideoBlob}
+          setGifBlob={setGifBlob}
+        />
       )}
-      <ReactHlsPlayer
-        autoPlay={false}
-        controls
-        width="30%"
-        height="auto"
-        crossOrigin="true"
-        src="https://d210c3znxi5wfh.cloudfront.net/assets/347434/HLS/347434.m3u8"
-        // type="application/x-mpegURL"
-      />
     </Section>
   );
 };
@@ -81,30 +100,64 @@ const Section = styled.div`
   width: 100vw;
   height: 90vh;
   padding: ${({ theme }) => theme.spacing.xxxl};
+  background-color: #73aace;
 `;
 
 const Container = styled.div`
   ${({ theme }) => theme.container.flexCenterColumn};
   width: 50%;
   height: 100%;
-  background-color: black;
-  transition: all 0.3s ease-out;
+  border-radius: 2em;
+  background-color: #bdbdbd;
+  opacity: 0;
+  transition: all 0.7s ease-out;
+
+  @keyframes appearAnimationGif {
+    from {
+      opacity: 0;
+      background-color: black;
+      transform: translateY(20px);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+      background-color: #bdbdbd;
+    }
+  }
+
+  &.show {
+    opacity: 1;
+    animation: appearAnimationGif 0.7s ease-in-out;
+  }
+`;
+
+const Text = styled.h2`
+  text-align: center;
+  font-family: "ABeeZee";
+  font-size: ${({ theme }) => theme.fontSizes.xxl};
+  font-weight: 600;
 `;
 
 const Gif = styled.img`
-  width: 560px;
-  height: 420px;
+  width: 70%;
+  height: 55%;
   border-radius: 0.5rem;
   margin: 40px auto;
+`;
+
+const ButtonContainer = styled.div`
+  ${({ theme }) => theme.container.flexCenter};
+  padding: ${({ theme }) => theme.spacing.xl};
 `;
 
 const Button = styled.button`
   width: fit-content;
   padding: ${({ theme }) => theme.spacing.xl};
+  margin-right: ${({ theme }) => theme.spacing.xxxl};
   border-radius: 0.5em;
   background-color: black;
   color: white;
-  font-family: sans-serif;
+  font-family: "ABeeZee";
   text-align: center;
 `;
 
