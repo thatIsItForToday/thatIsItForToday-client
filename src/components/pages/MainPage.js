9e42e2c1ff67ff3afb3db93f4a31b803eae1efe0
Seonghoon Oh <su5785@naver.com>
2022-03-13 00:14:34 +0900

    refactor: Add getting user vieos data logic when user is logged in on Mainpage component

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 
+import axios from "../../config/axiosInstance";
 import { IMAGE_URL } from "../../config/constants";
+import { videoActions } from "../../features/videoSlice";
 
 const MainPage = () => {
+  const dispatch = useDispatch();
+
+  const { user, isLoggedIn } = useSelector(state => state.user);
+
+  useEffect(async () => {
+    if (isLoggedIn) {
+      if (!isLoggedIn) {
+        return;
+      }
+
+      const { data } = await axios.get(`/users/${user.id}/videos`);
+
+      if (data.videos) {
+        const { videos } = data;
+
+        dispatch(videoActions.setVideos({ videos }));
+      }
+    }
+  }, [isLoggedIn]);
+
   return (
     <Section>
       <TextContainer>
